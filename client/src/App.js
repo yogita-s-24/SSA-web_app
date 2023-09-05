5af72149094781cf3c5b73e495d22de4209ff1a6
harshbhanger0 <harshbhange123@gmail.com>
2023-09-05 19:40:05 +0530

    clean the client directory

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import Header from "./components/Header";
 function App() {
   return (
-    <div className="flex h-screen  items-center justify-center">
-      <Header />
-    </div>
+   <>
+   hello world
+   </>
   );
 }
 
