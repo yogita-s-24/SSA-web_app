9e87786e46d6e53312c97afbecd8f3abf0f11360
harshbhanger0 <harshbhange123@gmail.com>
2023-09-05 20:48:06 +0530

    folder structure added and some basic files and routes set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import {
+  Admin,
+  Home,
+  LogIn,
+  Registration,
+  Test,
+} from "./components/pages/export";
 function App() {
   return (
-   <>
-   hello world
-   </>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/reg" element={<Registration />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/pages/export.js
@@ -0,0 +1,7 @@
+import Home from "./home/Home";
+import Admin from "./admin/Admin";
+import Test from "./test/Test";
+import LogIn from "./auth/login/LogIn";
+import Registration from "./auth/reg/Registration";
+
+export { Home, Admin, Test , LogIn, Registration};
