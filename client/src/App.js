83f096aba203709de8ca55064d8538573aaa8184
harshbhanger0 <harshbhange123@gmail.com>
2023-09-05 19:37:00 +0530

    tailwind added to project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="flex h-screen  items-center justify-center">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
