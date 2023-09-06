37c933a96b787c84910aef5755f6cc5f13c06ced
harshbhanger0 <harshbhange123@gmail.com>
2023-09-06 15:11:42 +0530

    nav bug resolved

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -7,61 +7,68 @@ import { RxCross1 } from "react-icons/rx";
 
 function Nav() {
   const [isClosed, setIsClosed] = useState(true);
-  const handalCollapse = () => {
-    setIsClosed(!isClosed);
-  };
 
   return (
     <>
       <nav
-        className={`sticky top-0 flex flex-col px-0 sm:flex-col md:flex-row md:bg-[#B9E9FC] md:px-4 `}
+        className={`sticky top-0 flex flex-col px-0 sm:flex-col md:flex-row md:bg-[#B9E9FC] md:px-4 md:py-3 `}
       >
         <div className="z-50  flex bg-[#B9E9FC] px-4 py-2 md:hidden">
           <button
             className="rounded-md px-3 py-2 text-2xl outline-none hover:bg-[#a7e6fe] focus:ring-2  active:ring-2"
-            onClick={handalCollapse}
+            onClick={() => {
+              setIsClosed(!isClosed);
+            }}
           >
             {isClosed ? <RiAlignJustify /> : <RxCross1 />}
           </button>
         </div>
-        <div
-          className={`relative top-0 -z-10 w-full bg-[#B9E9FC] transition-all duration-500 ease-linear md:static ${
-            isClosed ? "-top-60" : "top-0"
-          }`}
-        >
-          <ul className="absolute top-0 flex w-full flex-col bg-[#B9E9FC] px-8 text-sm md:static md:flex-row md:justify-end md:gap-16 md:text-lg">
+        <div className={`relative top-0 -z-10 w-full bg-[#B9E9FC]  `}>
+          <ul
+            className={`${
+              isClosed ? "-top-60" : "top-0"
+            } absolute flex w-full flex-col bg-[#B9E9FC] px-8 text-sm transition-all duration-500 ease-linear md:static md:static md:top-0 md:flex-row md:justify-end md:gap-16 md:text-lg`}
+          >
             <li className="my-2 me-auto px-3 py-1  hover:bg-[#a7e6fe]  md:m-0 md:hover:bg-inherit">
               <Link
-                className=" outline-none focus:ring-2 active:ring-2"
+                className=" text-stone-900 outline-none transition-all duration-100 ease-out hover:text-stone-600 focus:ring-2 active:ring-2"
                 to="/"
-                onClick={handalCollapse}
+                onClick={() => {
+                  setIsClosed(!isClosed);
+                }}
               >
                 Home
               </Link>
             </li>
             <li className="my-2 me-auto px-3 py-1  hover:bg-[#a7e6fe]  md:m-0 md:hover:bg-inherit">
               <Link
-                className=" outline-none focus:ring-2 active:ring-2"
+                className=" text-stone-900 outline-none transition-all duration-100 ease-out hover:text-stone-600 focus:ring-2 active:ring-2"
                 to="/admin"
-                onClick={handalCollapse}
+                onClick={() => {
+                  setIsClosed(!isClosed);
+                }}
               >
                 Admin
               </Link>
             </li>
             <li className="my-2 me-auto px-3 py-1  hover:bg-[#a7e6fe]  md:m-0 md:hover:bg-inherit">
               <Link
-                className=" outline-none focus:ring-2 active:ring-2"
+                className=" text-stone-900 outline-none transition-all duration-100 ease-out hover:text-stone-600 focus:ring-2 active:ring-2"
                 to="/test"
-                onClick={handalCollapse}
+                onClick={() => {
+                  setIsClosed(!isClosed);
+                }}
               >
                 Test
               </Link>
             </li>
             <li className="my-2 me-auto px-3 py-1  hover:bg-[#a7e6fe]  md:m-0 md:hover:bg-inherit">
               <Link
-                className=" outline-none focus:ring-2 active:ring-2"
+                className=" text-stone-900 outline-none transition-all duration-100 ease-out hover:text-stone-600 focus:ring-2 active:ring-2"
                 to="/log-in"
-                onClick={handalCollapse}
+                onClick={() => {
+                  setIsClosed(!isClosed);
+                }}
               >
                 Log In
               </Link>
