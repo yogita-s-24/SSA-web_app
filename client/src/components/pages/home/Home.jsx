661bfc12327ad1d297c16d1b32ac58832d933f51
PB220-DEVLOP <pratikbhoyar2002@gmail.com>
2023-09-09 23:42:08 +0530

    basic chamge

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import Nav from "../../nav/Nav";
-// import LogIn from "../auth/login/LogIn";
-import { Registration } from "../export";
 
 function Home() {
   return (
     <div>
       <Nav />
-      <Registration/>
     </div>
   );
 }
