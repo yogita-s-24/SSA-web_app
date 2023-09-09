5bb82e424a8d2797c3a28f1969f59d3d28396f57
PB220-DEVLOP <pratikbhoyar2002@gmail.com>
2023-09-09 23:33:16 +0530

    basic signup and login page

--- client/src/components/pages/auth/login/LogIn.jsx
+++ client/src/components/pages/auth/login/LogIn.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function LogIn() {
   return (
-    <div>LogIn</div>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="bg-white p-8 rounded-md shadow-md w-96 border border-gray-300">
+        <h2 class="text-2xl font-semibold mb-4 text-center">Login</h2>
+        <form>
+            <div class="mb-4">
+                <label for="username" class="block text-gray-600 font-medium">Username or Email</label>
+                <input type="text" id="username" name="username" class="w-full px-4 py-2 border rounded-md focus:ring focus:ring-blue-300"/>
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-600 font-medium">Password</label>
+                <input type="password" id="password" name="password" class="w-full px-4 py-2 border rounded-md focus:ring focus:ring-blue-300"/>
+            </div>
+            <button type="submit" class="w-full bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Login</button>
+        </form>
+    </div>
+</div>
+
   )
 }
 
--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Nav from "../../nav/Nav";
+// import LogIn from "../auth/login/LogIn";
+import { Registration } from "../export";
 
 function Home() {
   return (
     <div>
       <Nav />
+      <Registration/>
     </div>
   );
 }
